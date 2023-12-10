3e1da3ab16dd9a05f80c7157f2a232baa5cf3b5d
amir alavi <amiralavit@gmail.com>
2023-12-10 17:09:36 +0330

    feat: bootstrap and fontAwesome added to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import ReactDOM from "react-dom/client";
 
-function App() {
-    return <h1>Hello world amir</h1>;
-}
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
