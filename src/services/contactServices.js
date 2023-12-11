f1295a5292f6109aad617fb11d628dc759a5d107
amir alavi <amiralavit@gmail.com>
2023-12-11 12:09:43 +0330

    feat: contact services added to services directory

---
+++ src/services/contactServices.js
@@ -0,0 +1,23 @@
+import { http } from "#/helper/http";
+
+const ContactApi = {
+    async getAllContacts() {
+        return await http.get("passenger");
+    },
+    async getContactsWithLimit(limit = 10, skip = 0) {
+        return await http.get(`passenger?limit=${limit}&skip=${skip}`);
+    },
+    async getContact(contactId) {
+        return await http.get(`passenger?${contactId}`);
+    },
+    async searchContact(query, limit = 30) {
+        return await http.get(
+            `/passenger/?where=${
+                typeof query === "string"
+                    ? `first-name:{contains:${query}}`
+                    : `phoneNumber:{contains:${query}`
+            }` + `&sort=createdAt DESC&limit=${limit}`
+        );
+    },
+};
+export { ContactApi };
