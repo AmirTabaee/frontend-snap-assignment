93f01111aba473ab1827a40dd69eeca3a0f86e85
amir alavi <amiralavit@gmail.com>
2023-12-11 11:42:18 +0330

    feat: contact component created

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,75 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-const Contact = () => {
-    return <div>Contact</div>;
+import { CURRENT_LINE, CYAN, ORANGE, PURPLE, RED } from "../../helpers/colors";
+
+const Contact = ({ contact }) => {
+    return (
+        <div className="col-md-6">
+            <div
+                style={{ backgroundColor: CURRENT_LINE }}
+                className="card my-2"
+            >
+                <div className="card-body">
+                    <div className="row align-items-center d-flex justify-content-around">
+                        <div className="col-md-4 col-sm-4">
+                            <img
+                                src={contact?.photo}
+                                alt={contact?.fullname}
+                                style={{ border: `1px solid ${PURPLE}` }}
+                                className="img-fluid rounded"
+                            />
+                        </div>
+                        <div className="col-md-7 col-sm-7">
+                            <ul className="list-group">
+                                <li className="list-group-item list-group-item-dark">
+                                    Full name :{"  "}
+                                    <span className="fw-bold">
+                                        {contact?.fullname}
+                                    </span>
+                                </li>
+
+                                <li className="list-group-item list-group-item-dark">
+                                    Phone number:{"  "}
+                                    <span className="fw-bold">
+                                        {contact?.mobile}
+                                    </span>
+                                </li>
+
+                                <li className="list-group-item list-group-item-dark">
+                                    Email :{"  "}
+                                    <span className="fw-bold">
+                                        {contact?.email}
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center">
+                            <Link
+                                to={`/contacts/${contact?.id}`}
+                                className="btn my-1"
+                                style={{ backgroundColor: ORANGE }}
+                            >
+                                <i className="fa fa-eye" />
+                            </Link>
+
+                            <button
+                                className="btn my-1"
+                                style={{ backgroundColor: CYAN }}
+                            >
+                                <i className="fa fa-pencil" />
+                            </button>
+                            <button
+                                className="btn my-1"
+                                style={{ backgroundColor: RED }}
+                            >
+                                <i className="fa fa-trash" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Contact;
